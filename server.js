7d0c8cef18acecba123e914b226ede3ffadcdfcb
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-08-01 22:06:05 -0400

    [1]-Created ExpressJS server. [2]-Added functionality to handle requests for Animals. [3]-Deployed app to Heroku. [4]-Add functionality to handle requests for a specific Animal.

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -58,3 +62,12 @@ app.get("/api/animals", (req, res) => {
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 }); //tell server to listen for requests
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404); // if no record exists for the animal being searched for, the client receives a 404 error
+  }
+});
